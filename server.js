827df8ec6738e10365b5dc559c832bc2392926b9
pipe <felipe.parra@blackmind.cl>
2023-09-06 21:21:26 -0300

    listo la subida de imagen

--- server.js
+++ server.js
@@ -6,24 +6,59 @@ import cors from "cors"
 import multer from 'multer';
 import { fileURLToPath } from 'url';
 import { dirname, extname } from 'path';
-
+import { Joya } from "./Class/Joya.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+const app = express();
+const joya = new Joya();
 
 // app.use(cors());
 //punto 14 uso de estructura de carpetas
-import { Joya } from "./Class/Joya.js"
 
-const app = express();
-const joya = new Joya();
+// Configuración de multer para gestionar la carga de imágenes
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images'); // Carpeta donde se guardarán las imágenes
+    },
+    filename: (req, file, cb) => {
+      const fileExt = extname(file.originalname);
+      const filename = `${file.originalname}${Date.now()}${fileExt}`;
+      cb(null, filename); // Nombre de archivo único basado en la marca de tiempo
+    },
+  });
+  
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 3 * 1024 * 1024 }, // Límite de 3 MB para el tamaño del archivo
+  });
+// Ruta GET que devuelve el formulario HTML para cargar una imagen
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/upload.html');
+  });
+
+// Ruta POST para recibir y guardar la imagen
+app.post('/upload', upload.single('imagen'), async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send('Debes seleccionar una imagen para cargar.');
+    }
+  
+    // Aquí puedes realizar acciones adicionales con la imagen, como almacenar información en una base de datos.
+  
+    res.status(200).send('Imagen cargada con éxito.');
+    console.log('Imagen agregada a la carpeta images');
+  });
+  
+  // Carpeta estática para servir imágenes cargadas
+  app.use('/images', express.static('public/images'));
+
+
 //Aqui iria el JWT
 
 //ACA CONFIGURAMOS TODO EL JWT
 
-const token = jwt.sign(usuario, 'secreto', { expiresIn: '1h' });
+// const token = jwt.sign(usuario, 'secreto', { expiresIn: '1h' });
 
-console.log('JWT generado:', token);
+// console.log('JWT generado:', token);
 
 
 //Listas las rutas
@@ -84,19 +119,6 @@ app.put("/v1/joyas/:id", async (req, res)=>{
         res.sendStatus(400);
     }
 })
-
-//endpoint para subir imagenes
-app.post("/v1/joyas/imagen"async (req, res)=>{
-    if (token === true){
-
-    }else{
-        
-    }
-    
-})
-
-
-
 
 app.listen(3000, ()=>{console.log("Levantado puerto http://localhost:3000")});
 
